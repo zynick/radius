d1f86cdd321f6d15befebeafa6d03fc47cc86fe1
Zi-Yuan Chan <ziyuan@zynick.com>
2017-01-18 15:29:19 +0800

    cleanup

--- express.js
+++ express.js
@@ -3,42 +3,36 @@
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express = require('express');
-const logger = require('morgan');
+const logger = require('morgan'); // TODO use morgan and use debug, confusing..
 // const favicon = require('serve-favicon');
 const path = require('path');
 
-const routes = require('./routes/index');
-// const home = require('./routes/home');
-// const login = require('./routes/login');
-
 const app = express();
 const isProd = process.env.NODE_ENV === 'production';
+const routes = require('./routes/index');
 
 
 /* View Engine Setup */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+
+/* Express Stack Setup */
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', routes);
-// app.use('/home', home);
-// app.use('/login', login);
 
 
-/* Catch 404 and Forward to Error Handler */
+/* 404 & Error Handlers */
 app.use((req, res, next) => {
     const err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
-
-/* Error Handlers */
 app.use((err, req, res, next) => {
     res.status(err.status || 500);
     // only print stacktrace in development, hide in production
@@ -47,7 +41,6 @@ app.use((err, req, res, next) => {
         message: err.message,
         error: err
     });
-
 });
 
 
