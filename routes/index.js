ed84df3e857044dd00219b7cee9c8c72504e6e4d
Zi-Yuan Chan <ziyuan@zynick.com>
2017-02-10 17:21:32 +0800

    display all debugging information during development

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,7 @@ const isProduction = NODE_ENV === 'production';
 const { version } = require('../package.json');
 
 
-const routeMain = (req, res) => {
-    res.json(`Radius API Server v${version}`);
-};
-
-const routeMainDebug = (req, res) => {
+const routeDebug = (req, res, next) => {
     log('==========');
     log(`HEADERS: ${JSON.stringify(req.headers, null, 2)}`);
     log(`QUERY: ${JSON.stringify(req.query, null, 2)}`);
@@ -20,7 +16,7 @@ const routeMainDebug = (req, res) => {
     log(`BODY: ${JSON.stringify(req.body, null, 2)}`);
     // res.setHeader('Access-Control-Allow-Credentials', 'true');
     // res.setHeader('Access-Control-Allow-Origin', '*');
-    res.json(`Radius API Server v${version}`);
+    next();
 };
 
 const routeNotFound = (req, res, next) => {
@@ -40,12 +36,10 @@ const routeErrorHandlerJSON = (err, req, res, next) => {
 };
 
 
-
-if (isProduction) {
-    router.get('/', routeMain);
-} else {
-    router.all('/', routeMainDebug);
+if (!isProduction) {
+    router.use(routeDebug);
 }
+router.get('/', (req, res) => res.json(`Radius API Server v${version}`));
 router.use('/api', require('./api'));
 router.use(routeNotFound);             // TODO change make routeBadRequest?
 router.use(routeErrorHandlerJSON);
