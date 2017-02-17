fa9882b4c3bf4d8f7cbbd8f18ec8e2c8adf4ec88
Zi-Yuan <ziyuan@tideanalytics.com>
2017-02-17 16:20:15 +0800

    add docker build push

--- build.sh
+++ build.sh
@@ -4,3 +4,9 @@ docker build -t garnetalpha-on.azurecr.io/radius-acct:latest -f Dockerfile.acct 
 docker build -t garnetalpha-on.azurecr.io/radius-auth:latest -f Dockerfile.auth .
 docker build -t garnetalpha-on.azurecr.io/radius-boot:latest -f Dockerfile.boot .
 docker build -t garnetalpha-on.azurecr.io/radius-web:latest  -f Dockerfile.wapi .
+
+docker push garnetalpha-on.azurecr.io/radius-acct:latest
+docker push garnetalpha-on.azurecr.io/radius-auth:latest
+docker push garnetalpha-on.azurecr.io/radius-boot:latest
+docker push garnetalpha-on.azurecr.io/radius-web:latest
+
