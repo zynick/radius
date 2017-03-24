ff161d668a64e53dc1e09c70a6c346d78fefb946
Zi-Yuan Chan <ziyuan@zynick.com>
2017-03-24 20:50:27 +0800

    fuck myself wasted my whole day because of this

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 const {
     AAA_SECRET_KEY = 'FunnyTreeBlackDeceive1',
-    AD_HOST = 'connect-dev-api.tideanalytics.com',
+    AD_HOST = 'manager-api-dev.tideanalytics.com',
     AD_KEY = 'DuS6YR9aCMJc0345mPNAkAIzaSyCChtVm4UlxD4',
     MONGO = 'localhost:27017/radius',
     NODE_ENV = 'development',
