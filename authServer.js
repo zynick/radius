9dda36cb12d1999f107832b18606127bb6d4fbff
Zi-Yuan Chan <ziyuan@zynick.com>
2017-03-28 19:06:07 +0800

    refactor: spacing

--- authServer.js
+++ authServer.js
@@ -20,24 +20,24 @@ const server = dgram.createSocket('udp4');
 mongoose.Promise = global.Promise;
 mongoose.connect(`mongodb://${MONGO}`);
 mongoose.connection.on('error', err => {
-    logError(`unable to connect to database at ${MONGO}`);
-    logError(err);
+  logError(`unable to connect to database at ${MONGO}`);
+  logError(err);
 });
 glob.sync('./models/*.js')
-    .forEach(model => require(model));
+  .forEach(model => require(model));
 
 
 /* Start Server */
 server.on('listening', () => {
-    const address = server.address();
-    log(`listening on ${address.address}:${address.port}`);
+  const address = server.address();
+  log(`listening on ${address.address}:${address.port}`);
 });
 
 server.on('message', require('./controllers/authorization.js')(server));
 
 server.on('error', err => {
-    logError(err);
-    server.close();
+  logError(err);
+  server.close();
 });
 
 server.bind(1812);
