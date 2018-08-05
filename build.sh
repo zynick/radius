843e79df1dc37f75dc38af659350c212dea76008
Zi-Yuan Chan <ziyuan@zynick.com>
2018-08-05 07:20:02 +0000

    it doesn't matter anymore

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-docker build -t garnetalpha-on.azurecr.io/radius-acct:latest -f Dockerfile.acct .
-docker build -t garnetalpha-on.azurecr.io/radius-auth:latest -f Dockerfile.auth .
-
-docker push garnetalpha-on.azurecr.io/radius-acct:latest
-docker push garnetalpha-on.azurecr.io/radius-auth:latest
-
